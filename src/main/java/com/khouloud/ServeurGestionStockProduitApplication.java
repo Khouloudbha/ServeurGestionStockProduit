c59ee6cdbd64676852fba6549f8b2a3682c680c6
khouloudbha <khouloudbha8991@gmail.com>
2018-09-18 21:51:56 +0200

    first commit

---
+++ src/main/java/com/khouloud/ServeurGestionStockProduitApplication.java
@@ -0,0 +1,12 @@
+package com.khouloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStockProduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStockProduitApplication.class, args);
+	}
+}
